d1effb3ca75cec0112aa8db71b65e85fa672c5c3
ank010 <ank010@ucsd.edu>
2023-05-21 03:39:36 -0700

    Finish Explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -47,14 +47,27 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  let hasServiceWorker = ("serviceWorker" in navigator);
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  window.addEventListener("load", async function () {
+    if (hasServiceWorker) {
+      try {
+        // B3. TODO - Register './sw.js' as a service worker (The MDN article
+        //            "Using Service Workers" will help you here)
+        const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/", });
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        if (registration.active) {
+          console.log("Registration has Succeeded")
+        }
+      } catch (error) {
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.
+        console.log("Registration has failed");
+      }
+    }
+  });
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -118,11 +131,11 @@ async function getRecipes() {
           resolve(Newrecipes);
 
         }
-      } catch (err) {
+      } catch (error) {
         // A10. TODO - Log any errors from catch using console.error
-        console.log(err);
+        console.error(error);
         // A11. TODO - Pass any errors to the Promise's reject() function
-        reject(err);
+        reject(error);
       }
     }
   });
